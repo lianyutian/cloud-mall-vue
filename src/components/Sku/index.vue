df849d570d36e5d53be856892fc0b57257fb899f
lianyutian <1436159527@qq.com>
2024-12-08 20:10:38 +0800

    sku组件

---
+++ src/components/Sku/index.vue
@@ -0,0 +1,81 @@
+<script setup></script>
+
+<template>
+  <div class="goods-sku">
+    <dl v-for="item in goods.specs" :key="item.id">
+      <dt>{{ item.name }}</dt>
+      <dd>
+        <template v-for="val in item.values" :key="val.name">
+          <img
+            :class="{ selected: val.selected, disabled: val.disabled }"
+            @click="clickSpecs(item, val)"
+            v-if="val.picture"
+            :src="val.picture"
+          />
+          <span
+            :class="{ selected: val.selected, disabled: val.disabled }"
+            @click="clickSpecs(item, val)"
+            v-else
+          >
+            {{ val.name }}
+          </span>
+        </template>
+      </dd>
+    </dl>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@mixin sku-state-mixin {
+  border: 1px solid #e4e4e4;
+  margin-right: 10px;
+  cursor: pointer;
+
+  &.selected {
+    border-color: $xtxColor;
+  }
+
+  &.disabled {
+    opacity: 0.6;
+    border-style: dashed;
+    cursor: not-allowed;
+  }
+}
+
+.goods-sku {
+  padding-left: 10px;
+  padding-top: 20px;
+
+  dl {
+    display: flex;
+    padding-bottom: 20px;
+    align-items: center;
+
+    dt {
+      width: 50px;
+      color: #999;
+    }
+
+    dd {
+      flex: 1;
+      color: #666;
+
+      > img {
+        width: 50px;
+        height: 50px;
+        margin-bottom: 4px;
+        @include sku-state-mixin;
+      }
+
+      > span {
+        display: inline-block;
+        height: 30px;
+        line-height: 28px;
+        padding: 0 20px;
+        margin-bottom: 4px;
+        @include sku-state-mixin;
+      }
+    }
+  }
+}
+</style>
